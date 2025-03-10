f1919a19b46d192655696c3b764506bf6e46f4e4
alexandrularion <larion.a@yahoo.com>
2025-03-10 21:09:36 +0200

    Updated concepts for JavaScriptRO46

---
+++ JavaScriptRO46/concepts/3_commercial_project_template/src/main.jsx
@@ -0,0 +1,8 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <p>We will learn more once we start with REACT FRAMEWORK!</p>
+  </StrictMode>
+);
